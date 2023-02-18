7f2870b7c5b612d021ef3144076c94f0273ef9a5
BeslijaHana <125749415+BeslijaHana@users.noreply.github.com>
2023-02-18 19:36:11 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,7 @@
-let temp1 = 0
 radio.onReceivedNumber(function (receivedNumber) {
-    temp1 = input.temperature()
     basic.showNumber(receivedNumber)
 })
 input.onButtonPressed(Button.A, function () {
-    temp1 = input.temperature()
     radio.setGroup(105)
     radio.sendNumber(input.temperature())
 })
